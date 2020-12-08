98c441f3819ba94fad51fff3b8095db1f1a3c91c
abbirku <abbir.ku@gmail.com>
2020-12-09 01:19:37 +0600

    Add WebApp

--- AutofacImplementation/Infrastructure/InfrastructureModule.cs
+++ AutofacImplementation/Infrastructure/InfrastructureModule.cs
@@ -13,6 +13,35 @@ namespace Infrastructure
         private readonly int _maxVal;
 
         //Note in future we will pass connectionString & migrationAssemblyName for dbContext
+        //Ex:
+        /** 
+         * private readonly string _connectionString;
+         * private readonly string _migrationAssemblyName;
+         * public InfrastructureModule(string connectionString, string migrationAssemblyName)
+         * {
+         *      _connectionString = connectionString;
+         *      _migrationAssemblyName = migrationAssemblyName;
+         * }
+         * protected override void Load(ContainerBuilder builder)
+         * {
+         *      builder.RegisterType<ShopingContext>()
+         *          .WithParameter("connectionString", _connectionString)
+         *          .WithParameter("migrationAssemblyName", _migrationAssemblyName)
+         *          .InstancePerLifetimeScope();
+         * 
+         *      builder.RegisterType<ProductRepository>().As<IProductRepository>()
+         *          .InstancePerLifetimeScope();
+         *      builder.RegisterType<CategoryRepository>().As<ICategoryRepository>()
+         *          .InstancePerLifetimeScope();
+         *      builder.RegisterType<ShopingUnitOfWork>().As<IShopingUnitOfWork>()
+         *          .InstancePerLifetimeScope();
+         *      builder.RegisterType<PurchaseService>().As<IPurchaseService>()
+         *          .InstancePerLifetimeScope();
+         *  
+         *      base.Load(builder);
+         *  }
+         */
+
         public InfrastructureModule(int minVal, int maxVal)
         {
             _minVal = minVal;
